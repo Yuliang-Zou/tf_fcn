de8116808344bcd18cfd47cd475fbeb4335a7238
Yuliang-Zou <zouyuliang123@gmail.com>
2017-03-09 03:28:46 -0500

    Fix an issue in pooling, can use any size input now.

--- src/Dataloader.py
+++ src/Dataloader.py
@@ -14,7 +14,7 @@ The Dataloader for VOC2011 to load and preprocess input image and segmentation
 ground truth. (Only)
 """
 class Dataloader(object):
-	def __init__(self, split, batch_num):
+	def __init__(self, split, config):
 		# Validate split input
 		if split != 'train' and split != 'val' and split != 'trainval' and split != 'test':
 			raise Exception('Please enter valid split variable!')
@@ -28,9 +28,11 @@ class Dataloader(object):
 			self.img_list = f.read().rstrip().split('\n')
 
 		self.num_images = len(self.img_list)
-		self.batch_num = batch_num
 		self.temp_pointer = 0    # First idx of the current batch
 		self._shuffle()
+
+		self.batch_num = config['batch_num']
+		self.max_size = config['max_size']
 
 		# Create double side mappings
 		self.gray_to_rgb, self.rgb_to_gray = colormap()
@@ -66,10 +68,11 @@ class Dataloader(object):
 			temp_imName = [self._img_at(x) for x in temp_range]
 			temp_segName = [self._seg_at(x) for x in temp_range]
 			temp_map = [self.rgb_to_gray,]*process_size
+			temp_size = [self.max_size,]*process_size
 
 			p = multiprocessing.Pool(process_size)
 
-			temp_result = p.map(prep_run_wrapper, zip(temp_imName, temp_segName, temp_map))
+			temp_result = p.map(prep_run_wrapper, zip(temp_imName, temp_segName, temp_map, temp_size))
 			p.close()
 			p.join()
 
@@ -86,7 +89,7 @@ class Dataloader(object):
 
 """No shuffle, fix batch num to 1"""
 class Dataloader_test(Dataloader):
-	def __init__(self, split):
+	def __init__(self, split, config):
 		# Validate split input
 		if split != 'train' and split != 'val' and split != 'trainval' and split != 'test':
 			raise Exception('Please enter valid split variable!')
@@ -100,8 +103,10 @@ class Dataloader_test(Dataloader):
 			self.img_list = f.read().rstrip().split('\n')
 
 		self.num_images = len(self.img_list)
-		self.batch_num = 1
 		self.temp_pointer = 0    # First idx of the current batch
+
+		self.batch_num = 1
+		self.max_size = config['max_size']
 
 		# Create double side mappings
 		self.gray_to_rgb, self.rgb_to_gray = colormap()
@@ -110,7 +115,7 @@ class Dataloader_test(Dataloader):
 	def get_minibatch_at(self, i):
 		img_name = self._img_at(i)
 		seg_name = self._seg_at(i)
-		data = prep_im_for_blob(img_name, seg_name, self.rgb_to_gray)
+		data = prep_im_for_blob(img_name, seg_name, self.rgb_to_gray, self.max_size)
 		img_blob = data['im_blob']
 		# seg_blob = data['seg_blob']
 		mask = data['mask']
@@ -127,8 +132,8 @@ class Dataloader_test(Dataloader):
 
 """Small size dataloader"""
 class Dataloader_small(Dataloader):
-	def __init__(self, split, batch_num):
-		Dataloader.__init__(self, split, batch_num)
+	def __init__(self, split, config):
+		Dataloader.__init__(self, split, config)
 
 	"""Override"""
 	def get_next_minibatch(self):
@@ -171,9 +176,19 @@ class Dataloader_small(Dataloader):
 		return [img_blobs, seg_blobs, mask_blobs, ori_sizes]
 
 if __name__ == '__main__':
+	config = {
+	'batch_num':1, 
+	'iter':100000, 
+	'num_classes':21, 
+	'max_size':(640,640),
+	'weight_decay': 0.0005,
+	'base_lr': 0.001,
+	'momentum': 0.9
+	}
+
 	# dataloader = Dataloader('train', 10)
 	# minibatch = dataloader.get_next_minibatch()
-	dataloader = Dataloader_test('val')
+	dataloader = Dataloader('val', config)
 	minibatch = dataloader.get_next_minibatch()
 
 
